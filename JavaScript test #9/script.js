d68501f3c848127a38f4d36975096179c0ebf18b
isasopamena <157019237+isasopamena@users.noreply.github.com>
2024-01-18 13:58:19 +0100

    Add files via upload

---
+++ JavaScript test #9/script.js
@@ -0,0 +1,9 @@
+TweenMax.staggerFrom(
+    ".header", 1.2, {x: "-80", opacity: 0, ease: Power3.easeOut, delay: 1,}, 0.08);
+
+TweenMax.staggerFrom(
+    ".navbar > div", 0.8, {x: "-80", opacity: 0, ease: Power3.easeOut, delay: 1,}, 0.08);
+
+TweenMax.staggerFrom(
+    ".images > div", 0.8, {x: "-80", opacity: 0, ease: Power3.easeOut, delay: 1, }, 0.08);
+
